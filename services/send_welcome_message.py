0b3024ca6595ee733ed4639c9ea3bb570a7b96c6
diudiu62 <115522593@qq.com>
2025-03-14 10:31:12 +0800

    bug: 修复判读sysmsgtemplate逻辑

--- services/send_welcome_message.py
+++ services/send_welcome_message.py
@@ -1,7 +1,7 @@
 '''
 Author: diudiu62
 Date: 2025-03-10 18:13:18
-LastEditTime: 2025-03-12 17:14:35
+LastEditTime: 2025-03-14 10:18:35
 '''
 import asyncio
 from astrbot.api import logger
@@ -32,47 +32,49 @@ class SendMessage(BaseManager):
         # if any(note_bot_join_group in text for note_bot_join_group in notes_bot_join_group):  # 邀请机器人加入群聊
         #     logger.debug("不处理机器人加入群聊消息。")
         #     pass
-        sysmsgtemplate = text.find('.//sysmsgtemplate')
-        if sysmsgtemplate:
-            # 系统消息
-            try:
-                # 解析xml
-                xml_text = text.split(':\n', 1) if ':\n' in text else text
-                group_id = xml_text[0]
-                mes_data = ET.fromstring(xml_text[1])
 
-                invited_link = mes_data.find(
-                    ".//link[@name='names']//username")
-                invited_username = invited_link.text if invited_link is not None else "未知用户"
-                # 邀请的用户nickname
-                invited_link = mes_data.find(
-                    ".//link[@name='names']//nickname")
-                invited_nickname = invited_link.text if invited_link is not None else "未知用户"
-                # 处理被remark的nickname
-                if "_" in invited_nickname:
-                    # 找到最后一个下划线的位置
-                    last_underscore_index = invited_nickname.rfind("_")
-                    # 获取最后一个下划线后面的部分
-                    suffix = invited_nickname[last_underscore_index + 1:]
-                    # 指定要去掉的关键字数组
-                    keywords = self.accept_friend_config.get("keywords", [])
-                    # 如果后缀在关键字数组中，去掉最后一个下划线及其后面的关键字
-                    if suffix in keywords:
-                        invited_nickname = invited_nickname[:last_underscore_index]
+        try:
+            # 解析xml
+            xml_content = text.split(':\n', 1) if ':\n' in text else text
+            group_id = xml_content[0]
+            mes_data = ET.fromstring(xml_content[1])
 
-                welcome_msg = self.group_invitation_config.get(
-                    "group_welcome_msg", "")
-                if welcome_msg:
-                    delay = int(self.group_invitation_config.get(
-                        "group_welcome_msg_delay", 0))
-                    await asyncio.sleep(delay)
-                    if invited_nickname != "未知用户":
-                        self.client.post_text(
-                            self.appid, group_id, f'@{invited_nickname} {welcome_msg}', invited_username)
-                        logger.info(f"发送入群欢迎消息: {welcome_msg}")
+            sysmsgtemplate = mes_data.find('.//sysmsgtemplate')
+            if sysmsgtemplate is not None:
+                # 非群系统消息
+                return
+            invited_link = mes_data.find(
+                ".//link[@name='names']//username")
+            invited_username = invited_link.text if invited_link is not None else "未知用户"
+            # 邀请的用户nickname
+            invited_link = mes_data.find(
+                ".//link[@name='names']//nickname")
+            invited_nickname = invited_link.text if invited_link is not None else "未知用户"
+            # 处理被remark的nickname
+            if "_" in invited_nickname:
+                # 找到最后一个下划线的位置
+                last_underscore_index = invited_nickname.rfind("_")
+                # 获取最后一个下划线后面的部分
+                suffix = invited_nickname[last_underscore_index + 1:]
+                # 指定要去掉的关键字数组
+                keywords = self.accept_friend_config.get("keywords", [])
+                # 如果后缀在关键字数组中，去掉最后一个下划线及其后面的关键字
+                if suffix in keywords:
+                    invited_nickname = invited_nickname[:last_underscore_index]
 
-            except ET.ParseError as e:
-                logger.error(f"[gewechat] Failed to parse group join XML: {e}")
-                logger.error(text)
-                # Fall back to regular text handling
-                pass
+            welcome_msg = self.group_invitation_config.get(
+                "group_welcome_msg", "")
+            if welcome_msg:
+                delay = int(self.group_invitation_config.get(
+                    "group_welcome_msg_delay", 0))
+                await asyncio.sleep(delay)
+                if invited_nickname != "未知用户":
+                    self.client.post_text(
+                        self.appid, group_id, f'@{invited_nickname} {welcome_msg}', invited_username)
+                    logger.info(f"发送入群欢迎消息: {welcome_msg}")
+
+        except ET.ParseError as e:
+            logger.error(f"[gewechat] Failed to parse group join XML: {e}")
+            logger.error(text)
+            # Fall back to regular text handling
+            pass
